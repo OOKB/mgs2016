87c1860114b02a9af6f461f7071dbbadf1cd2536
Tim Lupfer <tim@timlupfer.com>
2016-01-06 11:55:52 -0600

    Default map center

--- src/components/Galleries/GalleryMap.js
+++ src/components/Galleries/GalleryMap.js
@@ -2,12 +2,23 @@ import React, { PropTypes } from 'react'
 import GoogleMap from 'google-map-react'
 
 function GalleryMap({ locations, settings }) {
+  const mapOptions = {
+    defaultCenter: {
+      lat: 34.347595,
+      lng: -112.156636,
+    },
+    zoom: 10,
+  }
   return (
-    <div className="google-map-container" style={{
-      height:'500px'
-    }
-    }>
-      <GoogleMap />
+    <div
+      className="google-map-container"
+      style={{
+        height: '500px',
+        position: 'relative',
+      }}
+    >
+      <GoogleMap {...mapOptions}>
+      </GoogleMap>
       Some text
     </div>
   )
