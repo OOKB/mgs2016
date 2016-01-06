e7f986cd3a2e7c5806240a6c559dd2961001f0b8
Kai Curry <kai@cape.io>
2016-01-06 16:04:12 -0600

    Add active class on selected gallery location.

--- src/components/Galleries/ListingItem.js
+++ src/components/Galleries/ListingItem.js
@@ -1,9 +1,10 @@
 import React, { PropTypes } from 'react'
+import classnames from 'classnames'
 
-function ListingItem({ building, name, street, zip, value }) {
+function ListingItem({ active, building, name, street, zip, value }) {
   const address = `${street}, ${zip}`
   return (
-    <li className={ value }>
+    <li className={classnames(value, { active })}>
       <h4 className="gallery">{ name }</h4>
       <div className="building">{ building }</div>
       <div className="address">
@@ -15,6 +16,13 @@ function ListingItem({ building, name, street, zip, value }) {
 
 ListingItem.propTypes = {
   name: PropTypes.string.isRequired,
+  alias: PropTypes.array,
+  value: PropTypes.string.isRequired,
+  street: PropTypes.string.isRequired,
+  zip: PropTypes.number.isRequired,
+  geoData: PropTypes.object.isRequired,
+  active: PropTypes.bool.isRequired,
+  togglePin: PropTypes.func.isRequired,
 }
 ListingItem.defaultProps = {
 }
