e888fc9731ca356ce367539653d1f66067c89efb
Jared VanderHook <jared.vanderhook@gmail.com>
2016-01-06 12:15:07 -0600

    no message

--- src/components/Galleries/GalleryMap.js
+++ src/components/Galleries/GalleryMap.js
@@ -2,13 +2,6 @@ import React, { PropTypes } from 'react'
 import GoogleMap from 'google-map-react'
 
 function GalleryMap({ locations, settings }) {
-  const mapOptions = {
-    defaultCenter: {
-      lat: 34.347595,
-      lng: -112.156636,
-    },
-    zoom: 10,
-  }
   return (
     <div
       className="google-map-container"
@@ -17,7 +10,7 @@ function GalleryMap({ locations, settings }) {
         position: 'relative',
       }}
     >
-      <GoogleMap {...mapOptions}>
+      <GoogleMap {...settings}>
       </GoogleMap>
       Some text
     </div>
