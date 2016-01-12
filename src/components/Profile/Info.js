6dc7ec11e0397fb0d9baa7154e68b7bb18bb8286
Jared VanderHook <jared.vanderhook@gmail.com>
2016-01-12 12:18:14 -0600

    Created Info.js component in Profile

---
+++ src/components/Profile/Info.js
@@ -0,0 +1,21 @@
+import React, { PropTypes } from 'react'
+
+import Details from './Details'
+import StudentNav from './StudentNav'
+import ProgramList from './ProgramList'
+
+function Info({ user }) {
+  return (
+    <div className="student-info">
+      <StudentNav user={user} />
+      <Details user={user} />
+      <ProgramList user={user} />
+    </div>
+  )
+}
+
+Info.PropTypes = {
+  user: PropTypes.object.isRequired,
+}
+
+export default Info
