da6396b8691c2fe8f9e0d2571958d63cba04f464
Kai Curry <kai@cape.io>
2016-01-12 13:29:47 -0600

    Simple link to student profile page

--- src/components/Students/Student.js
+++ src/components/Students/Student.js
@@ -1,14 +1,15 @@
 import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
 
-function Student({ name, program, locationName, photos, showDate, uid, ...rest }) {
+function Student({ name, program, locationName, photos, showDate, id, ...rest }) {
   return (
-    <li {...rest} id={uid} className="student three columns">
+    <li {...rest} id={id} className="student three columns">
       <div className="student-photo">
         <img className="photo-inactive" src={photos.inactive.url} alt={name.display} />
         <img className="photo-active" src={photos.active.url} alt={name.display} />
       </div>
       <ul className="list-reset">
-        <li className="name"><h4>{name.display}</h4></li>
+        <li className="name"><h4><Link to={`student/${id}`} >{name.display}</Link></h4></li>
         { program && <li className="program">{program.label}</li> }
         { locationName && <li className="location">{locationName}</li> }
         { showDate && <li className="show-date">{showDate}</li> }
