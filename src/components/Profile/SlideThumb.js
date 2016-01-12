220a808440b6b01a48d32d410924773e6a43bb1e
Jared VanderHook <jared.vanderhook@gmail.com>
2016-01-12 13:25:33 -0600

    created SlideThumb.js component in Profile.

---
+++ src/components/Profile/SlideThumb.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classnames from 'classnames'
+
+function slideThumb() {
+  return (
+    <li>
+    </li>
+  )
+}
+
+SlideThumb.propTypes = {
+
+}
+
+export default SlideThumb
