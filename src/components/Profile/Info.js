d7e83f5b05ba5d9b8c70c97eac0338678962dae7
Tim Lupfer <tim@timlupfer.com>
2016-01-12 16:09:20 -0600

    Add program list back

--- src/components/Profile/Info.js
+++ src/components/Profile/Info.js
@@ -2,14 +2,14 @@ import React, { PropTypes } from 'react'
 
 import Details from './Details'
 // import StudentNav from './StudentNav'
-// import ProgramList from './ProgramList'
+import ProgramList from './ProgramList'
 
 function Info({ user }) {
   return (
     <div className="student-info">
       {/* <StudentNav user={user} /> */}
       <Details user={user} />
-      {/* <ProgramList user={user} /> */}
+      <ProgramList user={user} />
     </div>
   )
 }
