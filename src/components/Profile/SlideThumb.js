d874a1ff1582a483cd11c35dd5674ede09381525
Tim Lupfer <tim@timlupfer.com>
2016-01-13 08:33:22 -0600

    Props and image for thumb

--- src/components/Profile/SlideThumb.js
+++ src/components/Profile/SlideThumb.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import classnames from 'classnames'
 
-function SlideThumb() {
+function SlideThumb({ classNames, pos, src, title }) {
   return (
-    <li>
+    <li
+      className={classnames(classNames)}
+    >
+      <img
+        src={src}
+        title={title}
+        alt={title}
+      />
     </li>
   )
 }
 
 SlideThumb.propTypes = {
-
+  src: PropTypes.string,
+  title: PropTypes.string,
+  pos: PropTypes.number,
+  classNames: PropTypes.object,
 }
 
 export default SlideThumb
