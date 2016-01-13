2d66d9573eaa4ff37f709b5eb91dee6cb5caa6e3
Tim Lupfer <tim@timlupfer.com>
2016-01-13 08:46:06 -0600

    Crude thumb styles for mockup

--- src/components/Profile/SlideThumb.js
+++ src/components/Profile/SlideThumb.js
@@ -5,11 +5,24 @@ function SlideThumb({ classNames, currentPosition, src, title }) {
   return (
     <li
       className={classnames(classNames)}
+      style={
+        /* Remove these inline styles. Mockup only */
+        {
+          display: 'inline-block',
+        }
+      }
     >
       <img
         src={src}
         title={title}
         alt={title}
+        style={
+          /* Remove these inline styles. Mockup only */
+          {
+            maxWidth: '200px',
+            height: 'auto',
+          }
+      }
       />
     </li>
   )
