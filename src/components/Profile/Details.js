541669002a9b9e071d44f4b2aeb6972b57a616d4
Kai Curry <kai@cape.io>
2016-01-13 11:10:19 -0600

    fix for when student doesn't have program

--- src/components/Profile/Details.js
+++ src/components/Profile/Details.js
@@ -4,12 +4,12 @@ import Contact from './Contact'
 
 function Details({ user }) {
   const { name, locationName, program, showDates } = user
-  const programName = program.label
+  const programName = program && program.label
   return (
     <div className="student-details">
       <h2>{ name.display }</h2>
       <ul className="show-details">
-        <li className="program"><h3>{programName}</h3></li>
+        { programName && <li className="program"><h3>{programName}</h3></li> }
         <li className="location">Showing in: {locationName}</li>
         <li className="show-dates">{showDates}</li>
       </ul>
