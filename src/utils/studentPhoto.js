d90727c16f110a62b9da1f5abc0f8dae77bc8886
Kristian Bjørnard <kristian.bjornard@gmail.com>
2016-01-15 15:35:14 -0500

    update image ratio

--- src/utils/studentPhoto.js
+++ src/utils/studentPhoto.js
@@ -1,5 +1,5 @@
 const gradientImg = 'http://dbox.cape.io/7192159/cape/micagradshow/images/blend.jpg'
-const baseSettings = '?w=300&h=300&fit=crop&crop=faces&sat=-100'
+const baseSettings = '?w=300&h=225&fit=crop&crop=faces&sat=-100'
 const activeSettings = `&bm=multiply&blend=${encodeURIComponent(gradientImg)}&balph=80&bs=inherit`
 
 // Decide what image settings to use.
@@ -8,6 +8,6 @@ export default function photoDisplay(photo, active) {
   return {
     url: photo.url.href + baseSettings + stateSettings,
     height: 300,
-    width: 300,
+    width: 225,
   }
 }
