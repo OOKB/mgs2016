85a876b69bbf5d4a6489c4004fc1e6e14d39a69e
Kristian Bjørnard <kristian.bjornard@gmail.com>
2016-01-14 20:37:53 -0500

    tweaks to reception location and classes

--- src/components/Schedule/ScheduleItem.js
+++ src/components/Schedule/ScheduleItem.js
@@ -23,52 +23,54 @@ function ScheduleItem({ active, dateStr, images, locations, togglePin }) {
     >
       <div className="container">
         <div className="group">
-          <div className="schedule-text">
-            <h4>{ dateStr }</h4>
-            { locations &&
-              <div className="locations">
-                {
-                  locations.map((location, index) => {
-                    let galleryLocations
-                    // If gallery locations are available and section is active
-                    // then generate a list
-                    if (location.showLocation && active) {
-                      galleryLocations = _.uniq(location.showLocation.map((gallery) => {
-                        return gallery
-                      }))
-                    }
-                    return (
-                      <span key={index}>
-                        <p>{location.name}</p>
-                        <p>{ galleryLocations &&
-                              galleryLocations.map((galleryLocation) => {
-                                return (
-                                  <span
-                                    onClick={() => handleClick(galleryLocation.location.value)}
-                                  >
-                                    {galleryLocation.location.name}
-                                  </span>
-                                )
-                              })
-                            }
-                        </p>
-                      </span>
-                    )
-                  })
-                }
+          <div className="four columns">
+            <div className="schedule-text">
+              <h4>{ dateStr }</h4>
+              { locations &&
+                <div className="locations">
+                  {
+                    locations.map((location, index) => {
+                      let galleryLocations
+                      // If gallery locations are available and section is active
+                      // then generate a list
+                      if (location.showLocation && active) {
+                        galleryLocations = _.uniq(location.showLocation.map((gallery) => {
+                          return gallery
+                        }))
+                      }
+                      return (
+                        <span key={index}>
+                          <p>{location.name}</p>
+                          <p>{ galleryLocations &&
+                                galleryLocations.map((galleryLocation) => {
+                                  return (
+                                    <span
+                                      onClick={() => handleClick(galleryLocation.location.value)}
+                                    >
+                                      {galleryLocation.location.name}
+                                    </span>
+                                  )
+                                })
+                              }
+                          </p>
+                        </span>
+                      )
+                    })
+                  }
+                </div>
+              }
+            </div>
+            { openingReception &&
+              <div className="reception">
+                {openingReception}
               </div>
             }
           </div>
           { images &&
-            <div className="schedule-images">
+            <div className="schedule-images eight columns">
               {
                 images.map((imgInfo, index) => <img key={index} src={imgInfo.src} />)
               }
-            </div>
-          }
-          { openingReception &&
-            <div>
-              {openingReception}
             </div>
           }
         </div>
