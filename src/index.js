99affc0475476cadc029ffb76ca17fd2371a3ee7
Kristian Bjornard <kb@ookb.co>
2016-08-04 15:06:50 -0400

    ?/

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom'
 // Add the latest es2016+ stuff that we love.
 import 'babel-polyfill'
 
-
 import configureStore from './redux/configureStore'
 // Root React component.
 import Root from './containers/Root'
@@ -14,7 +13,7 @@ const initialState = window.__data || {
     currentYear: new Date().getFullYear(),
   },
 }
-console.log('Date Issues 2')
+
 // Configure and create our Redux store.
 const store = configureStore(initialState)
 
